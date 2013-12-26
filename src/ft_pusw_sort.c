a44584b9ac2ce49e0e6c7fade09a58a23702ca2e
Alexandre Petit <alexandre.petit.vilapon@gmail.com>
2013-12-26 15:42:51 +0100

    Ajout d'une fonction permettant de detecter si une liste est triee

--- src/ft_pusw_sort.c
+++ src/ft_pusw_sort.c
@@ -6,10 +6,23 @@
 /*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/26 15:09:11 by alexandre         #+#    #+#             */
-/*   Updated: 2013/12/26 15:10:15 by alexandre        ###   ########.fr       */
+/*   Updated: 2013/12/26 15:40:45 by alexandre        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include "../includes/ft_push_swap.h"
 
+void	ft_pusw_sort_1(t_list **l_a, t_list **l_b)
+{
+	int	is_sort;
+
+	is_sort = 0;
+	if (l_a && !l_b)
+	{
+		while (is_sort == 0)
+		{
+
+		}
+	}
+}
